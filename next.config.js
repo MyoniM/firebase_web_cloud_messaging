80c9eaa2fe484eae8c6dae32633dceb36680d5c6
Yonatan <se.yonatan.merkebu@gmail.com>
2022-01-03 00:52:22 +0300

    _

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    apiKey: "AIzaSyCsg-8DxIkUi1rLlAvGZtLci0P9cVKBXpk",
-    authDomain: "pushnotification-9884a.firebaseapp.com",
-    messagingSenderId: "230935893768",
-    projectId: "pushnotification-9884a",
-    storageBucket: "pushnotification-9884a.appspot.com",
-    appId: "1:230935893768:web:212d0e57f15cfe3b09febd",
-    serverKey: "AAAANcTaFwg:APA91bErv0II70fegmzef3R9hHtlps8A89SNgwRmrDaxYOW4YNc27VZfmg45sThRVTeK1wEcTpuv8d1ynk_4E2fFX0tAlTNXfsdX6jhEMFGCxhD3s3vTR-S1imEhvj2YAMJf7sZEZuAp"
-  },
+  env: {},
 };
